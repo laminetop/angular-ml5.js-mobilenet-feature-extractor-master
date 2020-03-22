adca7f32390722977f31803982eef0304a68d718
Aly Gueye <aly.gueye@b-yond.com>
2020-03-21 23:22:27 -0400

    Ma premiere commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,38 @@ declare let ml5: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, AfterViewInit {
+  // Variable d'extraction de carasteristique du reseau CNN mobilenet
   public mobileNetFeatureExtractor;
+  // Varaible classifieur de carasteristique
   public featureClassifier;
+  // Variable label
   public label;
+  // Variable de confidence
   public confidence;
+  // Variable nouveau label
   public newLabel;
+  // Variable bar de progression
   public currentProgress = 0;
+  // Variable fonction de perte
   public loss: number;
+  // Variable nombre d'iteration
   public iteration: number;
-  @ViewChild('video')  public video: ElementRef;
-  @ViewChild('canvas')  public canvas: ElementRef;
+
+  // Variable static pour l'element de reference de notre capteur ou source d'images
+  @ViewChild('video', { static: true })  public video: ElementRef;
+  // Variable static pour l'element de reference minature
+  @ViewChild('canvas', { static: true })  public canvas: ElementRef;
+  // variable tableau capture qui capture tous les images fournies en minature ou les videos
   public captures: Array<any>;
+
+  // Constructeur avec initialisation du tableau de minature a vide
+  // Ce constructeur prend en parametre un contexte dexecution sur les mises ajour entre le composant et la page HTML
   constructor(private zone: NgZone) {
     this.captures = [];
   }
+
+  // la methode ngOnInit() qui cree notre reseau CNN d'extraction de carasteristique mobilenet
+  // il cree aussi notre classifieur qui classifie en entree les images venant de la camera
   ngOnInit(): void {
      this.mobileNetFeatureExtractor = ml5.featureExtractor('MobileNet', () => {
       this.featureClassifier = this.mobileNetFeatureExtractor.classification(this.video.nativeElement, () => {
@@ -27,28 +45,46 @@ export class AppComponent implements OnInit, AfterViewInit {
       });
     });
   }
+
+  // la methode addImage du classifieur de carasteristique qui prend comme parametre le label ou cible
+  // activation de la capture des flux d'images venant de la camera
   addImage() {
     this.featureClassifier.addImage(this.newLabel);
     this.capture();
   }
+
+  // Entrainement du modele featurembilenetextraction
   train() {
+    // initialisation du nombre d'iteration à 0 et aussi de la variable de fonction de perte a 0
     this.iteration = 0; this.loss = 0;
+    // Initialisation de la barre de progression à 0
     this.currentProgress = 0;
+    // la methode train() qui prend en parametre la fonction de perte qui est null
     this.featureClassifier.train((loss) => {
+      // si la valeur de la fonction de perte est null
       if (loss == null) {
+        // Nombre d'iteration fixé à 100
         this.iteration = 100;
+        // On classifie les nos features et affiche les resultats
         this.mobileNetFeatureExtractor.classify((e, r) => {
           this.gotResults(e, r);
         });
-      } else {
+      }
+      // sinon on execute notre zone
+        // on incremente la barre de progression
+        // ainsi que le nombre d'iteration
+        // et aussi la fonction de perte
+      else {
         this.zone.run(() => {
           ++this.currentProgress;
+
           ++this.iteration;
           this.loss = loss;
         });
       }
     });
   }
+  // taches d'initialisations supplementaires
   public ngAfterViewInit() {
     console.log(webkitURL);
     if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -58,17 +94,21 @@ export class AppComponent implements OnInit, AfterViewInit {
       });
     }
   }
-
+// Methode d'activation de la camera et extracttraction de l'image et sa conversion en minature
   public capture() {
     const context = this.canvas.nativeElement.getContext('2d').drawImage(this.video.nativeElement, 0, 0, 320, 240);
     this.captures.push(this.canvas.nativeElement.toDataURL('image/png'));
   }
 
+  // La methode d'affichage des resulats
   gotResults(err, results) {
+    // sil y'a erreur
     if (err) {
       console.log(err);
+      // s'il y'a pas erreur
     } else {
       this.zone.run(() => {
+        // On affiche les resultats dans notre cible de sortie
         this.label = results[0].label;
         this.confidence = results[0].confidence;
       });
